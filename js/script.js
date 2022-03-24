99226e72d90d808642bd1e0eff38c3902b309c92
King <zolotojznak@gmail.com>
2022-03-24 16:42:48 +0500

    clear coments

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // Активная стрелка
 const filter = document.querySelectorAll('.filter__text');
 
-// console.log(filter);
 filter.forEach(item => {
     item.addEventListener('click', event => {
         item.querySelector('.arrow').classList.toggle('arrow-rotate');
